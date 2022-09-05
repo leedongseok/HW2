da926c31e8f69ed758b5f17925c52d0b6522db9d
leedongseok <ehdtjr6682@naver.com>
2022-09-05 21:23:59 +0900

    modify main.cc

--- main.cc
+++ main.cc
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include "count.hh"
 using namespace std;
 
 int main() {
 	cout << "Hello, world!" << endl;
+	count_(1);
+	count_(2);
+	count_(3);
+	count_(4);
+	count_(5);
+	count_(6);
+	count_(7);
+	count_(8);
+	count_(9);
+	count_(10);
+	cout << "Start!!"<<endl;
+	count << "2016313097 이동석" << endl;
 	return 0;
 
 }
